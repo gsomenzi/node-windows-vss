9c3393c1dbc721d0f57c1eeda2024e0c5524140f
Guilherme Somenzi <guilherme@somenzi.me>
2018-04-03 08:29:40 -0300

    Readme updated to publish in npm

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # node-windows-vss
 Native bindings to take advantage of Microsoft Windows Volume Shadow Copies.
+
+## Support
+
+ * Node.js (tested on >= 8.x)
+ 
+## Installation
+
+To take advantage of Windows Volume Shadow Copies, we've some Windows requirements:
+ * The command must to be executed as administrator.
+ * Remember to check LUA.
+ * Shadow Copies Service must tu be enabled as automatic or default option.
+
+```sh
+> npm install node-windows-vss --save
+```
+
+## API
+
+### takeSnapshot(volume, callback)
+It takes a snapshot from a specified volume. Returns a callback with (Error err, String snapshotPath)
+
+```javascript
+const WindowsVss = require('node-windows-vss')
+
+WindowsVss.takeSnapshot("C:", (err, res) => {
+	if (err) return console.log(err)
+	return console.log('res', res)
+})
+```
--- index.js
+++ index.js
@@ -9,17 +9,15 @@ module.exports = {
  * @description Take a snapshot of a specific volume.
  * @param {string} volume Volume letter with or without slash. E.g: "C:" or "C:\".
  * @param {function} callback Callback function. (Err err, String snapshotPath).
+ * @returns {string} Returns snapshot path instead volume letter.
  */
 function takeSnapshot(volume, callback) {
 	if (typeof callback !== 'function') throw new TypeError('Callback function must to be a valid function.')
 	if (typeof volume !== 'string') return callback(new TypeError('Volume name must to be a valid string.'))
 	if (!regexpVolume.test(volume)) return callback(new Error('The volume you entered does not appear to be in a valid format.'))
 	addon.snapshot(volume, (err, snapshotPath) => {
-		if (err) {
-			let newError = new Error(err)
-			return callback(newError, null)
-		}
-		if (!snapshotPath) return callback(new Error('Fail while trying to take the snapshot.'))
+		if (err) return callback(new Error(err), null)
+		if (!snapshotPath) return callback(new Error('Unknown error while trying to take the snapshot.'))
 		return callback(null, snapshotPath)
 	})
 }
